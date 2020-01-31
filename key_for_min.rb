9d23e91a30adc0038c9be58e67039ea945956a95
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 23:19:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |name|
-  puts "name"
+  name_hash.each do |a, b|
+  a <=> b
   end
 end
