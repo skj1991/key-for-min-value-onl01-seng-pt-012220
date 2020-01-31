7ff75a8e8b23b4bd8ab6aaad380e834067e5e84e
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 23:23:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.select do |a, b|
-  a <=> b
-  end
+  name_a = name_hash.to_a
+
+    d_value= 1000
+    d_key= nil
+
+    name_a.each do |n|
+        if n[1] < d_value
+          d_value = n[1]
+          d_key = n[0]
+        end
+     end
+    d_key
 end
